7d4d34a91c313d8ba66eda34ec23ca4b66f0585d
Lenny0939 <lennyescott@gmail.com>
2025-08-14 12:05:04 +1000

    added naersk and a package

--- flake.nix
+++ flake.nix
@@ -2,50 +2,76 @@
   description = "Rust flake with nightly for bevy";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, flake-utils, rust-overlay, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      rust-overlay,
+      nixpkgs,
+      naersk,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = (import nixpkgs) { inherit system overlays; };
         dioxus-cli = pkgs.callPackage ./dioxus.nix { };
-      in with pkgs; {
+        naerskLib = pkgs.callPackage naersk { };
+        dependencies = with pkgs; [
+          udev
+          alsa-lib
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          wayland
+          pkg-config
+          clang
+          mold
+          dioxus-cli
+        ];
+        rust = pkgs.rust-bin.selectLatestNightlyWith (
+          toolchain:
+          toolchain.default.override {
+            extensions = [
+              "rustc-codegen-cranelift-preview"
+              "rust-src"
+              "rust-analyzer"
+            ];
+            targets = [
+              "x86_64-pc-windows-msvc"
+              "wasm32-unknown-unknown"
+            ];
+          }
+        );
+      in
+      with pkgs;
+      {
         devShells.default = mkShell rec {
-          nativeBuildInputs = [
-            (rust-bin.selectLatestNightlyWith (toolchain:
-              toolchain.default.override {
-                extensions = [
-                  "rustc-codegen-cranelift-preview"
-                  "rust-src"
-                  "rust-analyzer"
-                ];
-                targets = [ "x86_64-pc-windows-msvc" "wasm32-unknown-unknown" ];
-              }))
-          ];
-          buildInputs = [
-            udev
-            alsa-lib
-            vulkan-loader
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-            libxkbcommon
-            wayland
-            pkg-config
-            clang
-            mold
-            cargo-xwin
-            wasm-bindgen-cli
-            binaryen
-            dioxus-cli
-          ];
-          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          buildInputs = dependencies;
+          nativeBuildInputs = [ rust ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath dependencies;
           BEVY_ASSET_ROOT = ".";
         };
-      });
+        packages.default =
+          (naerskLib.override {
+            cargo = rust;
+            rustc = rust;
+          }).buildPackage
+            {
+              src = ./.;
+              buildInputs = dependencies;
+              nativeBuildInputs = [ rust ];
+              cargoBuildOptions = default: default ++ [ "--no-default-features" ];
+            };
+      }
+    );
 }
