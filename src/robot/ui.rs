af23d93135024dd536dd09876e59b625cdbc1628
Lenny0939 <lennyescott@gmail.com>
2025-09-04 18:14:39 +1000

    ACTUALLY properly added in bleep 06 sound (and 01)

--- src/robot/ui.rs
+++ src/robot/ui.rs
@@ -141,15 +141,11 @@ fn button_system(
             }
             Interaction::Hovered => {
                 *color = HOVER_BUTTON.into();
-                commands.spawn(SamplePlayer::new(
-                    asset_server.load("packs/Interfaces_Bleeps/Bleep_06.wav"),
-                ));
+                commands.spawn(SamplePlayer::new(asset_server.load("audio/Bleep_06.wav")));
             }
             Interaction::Pressed => {
                 *color = HOVER_BUTTON.into();
-                commands.spawn(SamplePlayer::new(
-                    asset_server.load("audio/Interfaces_Bleeps/Bleep_01.wav"),
-                ));
+                commands.spawn(SamplePlayer::new(asset_server.load("audio/Bleep_01.wav")));
                 ev_click.write(ClickEvent(*button_type));
             }
         }
